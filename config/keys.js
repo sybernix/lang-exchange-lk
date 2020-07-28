ab7e6f0da698cddfa9e686f63d024223d94d51ad
niruhan <niruhanv@gmail.com>
2020-07-28 10:53:34 +0530

    Initial work commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:
+        'mongodb://localhost:27017/lang-exchange-lk?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false',
+    secretOrKey: 'secret',
+};
