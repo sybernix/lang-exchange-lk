6f7200f5c9909fd255768f7546c026c894944bf0
niruhan <niruhanv@gmail.com>
2020-08-09 14:24:01 +0530

    Get user list and log in discover

--- client/src/Components/Discover/Discover.jsx
+++ client/src/Components/Discover/Discover.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types'
+import { useGetUsers } from '../../Services/userService';
 
 const Discover = props => {
+    const getUsers = useGetUsers();
+
+    // console.log({getUsers});
+
+    useEffect(() => {
+        getUsers().then(res => console.log(res));
+    }, null);
+
     return (
         <div>
+            {/*console.log(getUsers.getUsers());*/}
+            {/*getUsers().then(res => setUsers(res));*/}
             <h3>Welcome to Discover Page</h3>
             <Link to="/chat">chat</Link>
         </div>
@@ -16,3 +27,27 @@ Discover.propTypes = {
 }
 
 export default Discover
+
+// import React, {Component} from 'react';
+// import {Link} from "react-router-dom";
+// import { useGetUsers } from '../../Services/userService';
+//
+// class Discover extends Component {
+//
+//     componentDidMount() {
+//         useGetUsers((response) => {
+//             console.log(response);
+//         });
+//     }
+//
+//     render() {
+//         return (
+//             <div>
+//                 <h3>Welcome to Discover Page</h3>
+//                 <Link to="/chat">chat</Link>
+//             </div>
+//         );
+//     }
+// }
+//
+// export default Discover;
