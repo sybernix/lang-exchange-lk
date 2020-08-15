5e9f67d867e89c69d262f1f19fe3c327c55f3803
niruhan <niruhanv@gmail.com>
2020-08-15 15:26:22 +0530

    Change implementation

--- config/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongoURI:
-        'mongodb://localhost:27017/lang-exchange-lk?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false',
-    secretOrKey: 'secret',
-};
