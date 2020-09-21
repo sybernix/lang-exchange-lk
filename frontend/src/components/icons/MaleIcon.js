a5b22de6912978df2536801fbf7a9016eb4192f8
niruhan <niruhanv@gmail.com>
2020-09-21 23:41:51 +0530

    Clean male icon svg

--- frontend/src/components/icons/MaleIcon.js
+++ frontend/src/components/icons/MaleIcon.js
@@ -12,16 +12,11 @@ export const MaleIcon = ({ width, color }) => {
     const DEFAULT_COLOR = theme.colors.text.secondary;
 
     return (
-        // <svg
-        //     // enable-background="new 0 0 24 24"
-        //     // height="30"
-        //     viewBox="0 0 24 24"
-        //     width={width || DEFAULT_WIDTH}
-        //     xmlns="http://www.w3.org/2000/svg"
-        //     fill={theme.colors[color] || DEFAULT_COLOR}
-        // >
         <svg
-            viewBox="0 0 49.267 49.267">
+            width={width || DEFAULT_WIDTH}
+            viewBox="0 0 49.267 49.267"
+            fill={theme.colors[color] || DEFAULT_COLOR}
+        >
             <path d="M31.261,22.368l11.838-11.838v12.602c0,0.851,0.345,1.623,0.903,2.181s1.33,0.904,2.181,0.903
 	c1.704,0,3.085-1.381,3.085-3.085V3.084C49.268,1.382,47.886-0.001,46.183,0L26.135,0c-1.704,0-3.085,1.381-3.085,3.085
 	c-0.001,1.702,1.382,3.085,3.084,3.084h12.602L26.899,18.006c-6.682-4.621-15.938-3.964-21.882,1.981
