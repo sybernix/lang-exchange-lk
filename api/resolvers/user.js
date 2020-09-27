7b3425d44de24a46b8aea7da5b17aa29663f5dcf
niruhan <niruhanv@gmail.com>
2020-09-27 15:10:08 +0530

    Use city to resolve gql

--- api/resolvers/user.js
+++ api/resolvers/user.js
@@ -244,7 +244,7 @@ const Query = {
      * @param {int} skip how many users to skip
      * @param {int} limit how many users to limit
      */
-    getPotentialPartners: async (root, {userId, skip, limit}, {User, Follow}) => {
+    getPotentialPartners: async (root, {userId, city, skip, limit}, {User, Follow}) => {
         // Find user ids, that current user follows
         const userFollowing = [];
         const follow = await Follow.find({follower: userId}, {_id: 0}).select(
@@ -260,7 +260,7 @@ const Query = {
 
         // Find users that user is not following
         const query = {
-            $and: [{_id: {$ne: userId}}, {_id: {$nin: userFollowing}}, {targetLanguage: currentUser["0"].nativeLanguage}, {nativeLanguage: currentUser["0"].targetLanguage}],
+            $and: [{_id: {$ne: userId}}, {_id: {$nin: userFollowing}}, {targetLanguage: currentUser["0"].nativeLanguage}, {nativeLanguage: currentUser["0"].targetLanguage}, {city: city}],
         };
         const count = await User.where(query).countDocuments();
         const users = await User.find(query)
