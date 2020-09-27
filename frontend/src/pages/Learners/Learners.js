b62e91439b1117eb9635a7398018bba6ba634451
niruhan <niruhanv@gmail.com>
2020-09-27 14:42:04 +0530

    Add location select box

--- frontend/src/pages/Learners/Learners.js
+++ frontend/src/pages/Learners/Learners.js
@@ -7,6 +7,8 @@ import {Loading} from 'components/Loading';
 import Empty from 'components/Empty';
 import InfiniteScroll from 'components/InfiniteScroll';
 import Head from 'components/Head';
+import {Select} from 'components/Form';
+import {H3} from 'components/Text';
 import LearnerCard from './LearnerCard';
 
 import {GET_POTENTIAL_PARTNERS} from 'graphql/user';
@@ -19,6 +21,16 @@ import {Query} from 'react-apollo';
 
 const Root = styled(Container)`
   margin-top: ${p => p.theme.spacing.lg};
+
+  @media (min-width: ${p => p.theme.screen.lg}) {
+    margin-left: ${p => p.theme.spacing.lg};
+    padding: 0;
+  }
+`;
+
+const Filters = styled(Container)`
+  /* margin-top: ${p => p.theme.spacing.sm}; */
+  margin-bottom: ${p => p.theme.spacing.lg};
 
   @media (min-width: ${p => p.theme.screen.lg}) {
     margin-left: ${p => p.theme.spacing.lg};
@@ -48,7 +60,27 @@ const Learners = () => {
     return (
         <Root maxWidth="md">
             <Head title="Find language partners"/>
-
+            <Filters>
+                <H3>Search learners by:</H3>
+                <Select
+                    type="text"
+                    name="city"
+                    // defaultValue={data.getUser.city}
+                    // values={city}
+                    // onChange={handleChange}
+                    borderColor="white"
+                >
+                    {
+                        ["Ampara", "Anuradhapura", "Badulla", "Batticaloa", "Colombo", "Galle", "Gampaha", 
+                        "Hambantota", "Jaffna", "Kalutara", "Kandy", "Kegalle", "Kilinochchi", "Kurunegala", 
+                        "Mannar", "Matale", "Matara", "Monaragala", "Mullaitivu", "NuwaraEliya", "Polonnaruwa", 
+                        "Puttalam", "Ratnapura", "Trincomalee", "Vavuniya"]
+                        .map((year, index) => {
+                            return <option key={`year${index}`} value={year}>{year}</option>
+                        })
+                    }
+                </Select>
+            </Filters>
             <Query
                 query={GET_POTENTIAL_PARTNERS}
                 variables={variables}
