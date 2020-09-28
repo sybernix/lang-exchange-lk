a4cfaf4e96a84fa7a22c6edf36aa14461fcac6b3
niruhan <niruhanv@gmail.com>
2020-09-28 22:55:59 +0530

    Add sex filter label

--- frontend/src/pages/Learners/Learners.js
+++ frontend/src/pages/Learners/Learners.js
@@ -106,18 +106,21 @@ const Learners = () => {
                         </Select>
                     </Filter>
                     <Filter>
-                    <Select
-                        type="text"
-                        name="sex"
-                        // defaultValue={data.getUser.sex}
-                        // values={sex}
-                        onChange={handleChange}
-                        borderColor="white"
-                    >
-                        <option value="male">Male</option>
-                        <option value="female">Female</option>
-                        <option value="other">Other</option>
-                    </Select>
+                        Sex:
+                    </Filter>
+                    <Filter>
+                        <Select
+                            type="text"
+                            name="sex"
+                            // defaultValue={data.getUser.sex}
+                            // values={sex}
+                            onChange={handleChange}
+                            borderColor="white"
+                        >
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                            <option value="other">Other</option>
+                        </Select>
                     </Filter>
                 </FilterBase>
             </FilterContainer>
