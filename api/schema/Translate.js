d1996c3f1616353cb183bd93d5ae4f98da20ba36
niruhan <niruhanv@gmail.com>
2020-10-11 15:11:38 +0530

    Add translate schema

---
+++ api/schema/Translate.js
@@ -0,0 +1,23 @@
+import {gql} from 'apollo-server-express';
+
+/**
+ * Translate schema
+ */
+const TranslateSchema = gql`
+  # ---------------------------------------------------------
+  # Return Payloads
+  # ---------------------------------------------------------
+  type TranslatePayload {
+    translatedText: [PostPayload]!
+    language: String!
+  }
+
+  # ---------------------------------------------------------
+  # Queries
+  # ---------------------------------------------------------
+  extend type Query {
+    # Gets user posts by username
+    getTranslation(text: String!, fromLang: String, toLang: String!): TranslatePayload
+`;
+
+export default TranslateSchema;
