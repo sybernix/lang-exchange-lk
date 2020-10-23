0d72f5adb605e280c4f9a7883097176baea03ba7
niruhan <niruhanv@gmail.com>
2020-10-23 16:01:11 +0530

    Return null if not authenticated

--- backend/resolvers/user.js
+++ backend/resolvers/user.js
@@ -40,7 +40,9 @@ const Query = {
                 ],
                 match: {seen: false},
             });
-
+        if (user == null) {
+            return null;
+        }
         user.newNotifications = user.notifications;
 
         // Find unseen messages
