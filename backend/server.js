047a6309ad8c88200751fc40f3f1e9cfcc45d1db
niruhan <niruhanv@gmail.com>
2020-10-28 11:04:05 +0530

    Allow multiple frontend urls in cors

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const app = express();
 
 // Enable cors
 const corsOptions = {
-    origin: process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL.split(','),
     credentials: true,
 };
 app.use(cors(corsOptions));
